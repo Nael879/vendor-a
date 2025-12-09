b5c6d449edbf38b6a685d96cd2e1a1f89cc69c46
Nailatul <nailatul465@gmail.com>
2025-12-09 12:49:24 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+
+import express from "express";
+import { pool } from "./db.js";
+
+const app = express();
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const q = "SELECT * FROM vendor_a.products ORDER BY id ASC";
+    const { rows } = await pool.query(q);
+    res.json(rows);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+export default app;
+
+console.log("Connected URL:", process.env.DATABASE_URL);
+
